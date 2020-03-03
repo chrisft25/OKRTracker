25d3deeae58c372e339f618589a585d45fc77da3
Christopher Fuentes <fuenteschris75@gmail.com>
2020-03-03 10:31:08 -0600

    Adding other boards to the tracker

--- src/components/Task/index.jsx
+++ src/components/Task/index.jsx
@@ -11,78 +11,92 @@ const trello = new Trello({
 const Task = () => {
   const [tasks, setTasks] = useState([]);
   const members = [];
-  let cards = [];
+  const cards = [];
 
-  const getMembers = async (idBoard = process.env.REACT_APP_MAIN_BOARD) => {
-    const member = await (
-      await trello
-        .boards(idBoard)
-        .members()
-        .getMembers()
-    ).json();
+  const getMembers = async (idBoards = (process.env.REACT_APP_MAIN_BOARD).split(",")) => {
+    let member = [];
+    let preData = [];
+    console.log(idBoards);
+    await Promise.all(idBoards.map(async (idBoard) => {
+      preData = await (
+        await trello
+          .boards(idBoard)
+          .members()
+          .getMembers()
+      ).json();
+      preData.map((val) => { member = [...member, val]; });
+    }));
+
+    member = member.filter((mem, key) => key === member.indexOf(mem));
+    console.log(member);
 
     await getAvatars(member);
-    getDoingCards();
   };
 
-  const getAvatars = async member =>
-    Promise.all(
-      member.map(async mem => {
-        const { id, fullName, username } = mem;
-        const { _value } = await (
-          await trello.members(mem.id).getFieldValue("avatarUrl")
-        ).json();
-        members.push({
-          id,
-          fullName,
-          username,
-          avatar: _value,
-        });
-      })
-    );
-
-  const getDoingCards = async () => {
-    await (
-      await trello
-        .lists("5e55bf69d7c0996f4c7a4b6e")
-        .cards()
-        .getCards()
-    )
-      .json()
-      .then(data => {
-        cards = data.map(card => ({
-          id: card.idMembers[0],
-          task: card.name,
-          img: members.filter(mem => mem.id === card.idMembers[0])[0]
-            ? `${
-                members.filter(mem => mem.id === card.idMembers[0])[0].avatar
-              }/original.png`
-            : null,
-          name: members.filter(mem => mem.id === card.idMembers[0])[0]
-            ? members.filter(mem => mem.id === card.idMembers[0])[0].fullName
-            : null,
-        }));
+  const getAvatars = async (member) => Promise.all(
+    member.map(async (mem) => {
+      const { id, fullName, username } = mem;
+      const { _value } = await (
+        await trello.members(mem.id).getFieldValue("avatarUrl")
+      ).json();
+      members.push({
+        id,
+        fullName,
+        username,
+        avatar: _value,
       });
+    }),
+  );
+
+  const getDoingCards = async (idLists = (process.env.REACT_APP_DOING_LISTS).split(",")) => {
+    await Promise.all(idLists.map(async (idList) => {
+      await (
+        await trello
+          .lists(idList)
+          .cards()
+          .getCards()
+      )
+        .json()
+        .then((data) => {
+          data.map((card) => cards.push({
+            id: card.idMembers[0],
+            task: card.name,
+            img: members.filter((mem) => mem.id === card.idMembers[0])[0]
+              ? `${
+                members.filter((mem) => mem.id === card.idMembers[0])[0].avatar
+              }/original.png`
+              : null,
+            name: members.filter((mem) => mem.id === card.idMembers[0])[0]
+              ? members.filter((mem) => mem.id === card.idMembers[0])[0].fullName
+              : null,
+          }));
+        });
+    }));
+
+    console.log(cards);
     setTasks(cards);
   };
 
   useEffect(() => {
-    getMembers();
+    async function initialData() {
+      await getMembers();
+      await getDoingCards();
+    }
+
+    initialData();
   }, []);
 
   return (
     <>
       <div className="container-fluid">
-        {tasks.map(task =>
-          task.id ? (
-            <div className="row height-50 bounceInUp animated" key={task.id}>
-              <UserCard user={task} />
-              <TaskCard task={task.task} />
-            </div>
-          ) : (
-            ""
-          )
-        )}
+        {tasks.map((task) => (task.id ? (
+          <div className="row height-50 bounceInUp animated" key={task.id}>
+            <UserCard user={task} />
+            <TaskCard task={task.task} />
+          </div>
+        ) : (
+          ""
+        )))}
       </div>
     </>
   );
